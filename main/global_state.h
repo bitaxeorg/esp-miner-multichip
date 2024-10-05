7cf2ffa8cead9433ce7afb93dedde93e5253a257
shufps <shufps80@gmail.com>
2024-10-05 06:31:02 +0200

    reverts to old hashrate calculation

--- main/global_state.h
+++ main/global_state.h
@@ -41,9 +41,11 @@ typedef struct
 
 typedef struct
 {
+    double duration_start;
     int historical_hashrate_rolling_index;
-    uint64_t historical_hashrate_time_stamps[HISTORY_LENGTH];
+    double historical_hashrate_time_stamps[HISTORY_LENGTH];
     double historical_hashrate[HISTORY_LENGTH];
+    int historical_hashrate_init;
     double current_hashrate;
     double current_hashrate_10m;
     double current_hashrate_1h;
